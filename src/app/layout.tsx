c21717404381e492c77cd40be9716f4e39a1dcfb
ChonkyKet <160700671+TannerCat@users.noreply.github.com>
2024-04-03 22:45:30 +0800

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,15 +11,17 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
   // Wrapping the content in a div with a background container class for styling
   return (
-    <>
-    <div className={styles.backgroundContainer}>
-      <header className={styles.header}>
-        <img src="/images/scc.png" alt="Company Seal" className={styles.seal} />
-        <h1 className={styles.fancyTitle}>SCC OJT BLOG</h1>
-      </header>
-      <Sidebar />
-      <main className={styles.mainContent}>{children}</main>
-    </div>
-    </>
+    <html lang="en">
+      <body>
+        <div className={styles.backgroundContainer}>
+          <header className={styles.header}>
+            <img src="/images/scc.png" alt="Company Seal" className={styles.seal} />
+            <h1 className={styles.fancyTitle}>SCC OJT BLOG</h1>
+          </header>
+          <Sidebar />
+          <main className={styles.mainContent}>{children}</main>
+        </div>
+      </body>
+    </html>
   );
 }
